a96a88a884518f231319f4dca6fe9c2780423a85
anna <anna.soderlund@ha.ax>
2024-04-04 14:05:07 +0300

    Modified and tested add to take strings with new line

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -3,7 +3,7 @@ public class StringCalculator {
     public int add(String input) {
         int sum=0;
         if(!input.isEmpty()) {
-            String [] numbers = input.split(",");
+            String [] numbers = input.split("[, \n]");
             for (String number : numbers) {
                 sum += Integer.parseInt(number);
             }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -40,4 +40,9 @@ public class StringCalculatorTest {
     public void testTenNumbers(){
         Assertions.assertEquals(10,calculator.add("1,1,1,1,1,1,1,1,1,1"));
     }
+
+    @Test
+    public void testStringWithNewRow(){
+        Assertions.assertEquals(10,calculator.add("2\n5,3"));
+    }
 }
